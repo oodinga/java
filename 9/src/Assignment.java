363df52f3587f653eb67f891a19bb7eaccc37ba6
odinga96 <david.odinga@gmail.com>
2022-01-27 23:40:23 +0300

    Solved newChallenge Given a String of integers, find the maximum number of numbers divisible by 3 given that you can make at most one change:
    Example
    Given 21
    
    Numbers divisible by 1 would be
    21 24 27 51 81

--- 9/src/Assignment.java
+++ 9/src/Assignment.java
@@ -2,7 +2,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.StringJoiner;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author : Odinga David
@@ -41,8 +44,34 @@ public class Assignment {
     }
 
 
+
+    public static int Solution(String S){
+        int count =Integer.parseInt(S) % 3 == 0? 1: 0;
+        for (int i= 0; i< S.length(); i++){
+            int index = i;
+
+            Predicate<Integer> isDivisibleBy3 = n -> n%3 == 0;
+            Function<Integer, String>  replaceNumber = s -> {
+                StringBuilder str = new StringBuilder(S);
+                str.setCharAt(index, (char)(s+'0'));
+                return str.toString();
+            };
+            Function<String, Integer> convertToInt = Integer::parseInt;
+
+            count += (Long) Stream.iterate(0, n -> n < 10, n -> n+1)
+                    .filter(num ->S.charAt(index) != (char)(num+'0'))
+                    .map(replaceNumber.andThen(convertToInt))
+                    .filter(isDivisibleBy3)
+                    .count();
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(palindrome(6, 3));
+//        System.out.println(palindrome(6, 3));
+
+        System.out.println(Solution("0081"));
     }
 
 }
